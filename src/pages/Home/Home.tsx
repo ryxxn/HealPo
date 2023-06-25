9cebac4c930d268ae56052f162488b1842668d17
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-06-26 04:45:14 +0900

    Home 페이지 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,46 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Home.css';
+import './Home.scss';
 import { Header } from '../../components/Header';
-import ExploreContainer from '../../components/ExploreContainer';
+import { Container } from '../../components/Container';
+import { Card } from '../../components/Card';
+
+type recommandDataType = {
+  name: string,
+  iconNum: number,
+  id: string
+}
 
 const Home: React.FC = () => {
+
+  const recommandData: recommandDataType[] = [{ name: "5 X 5 세트", iconNum: 11, id: "a" }, { name: "Interval Training", iconNum: 4, id: "b" }, { name: "10 X 10 세트", iconNum: 3, id: "c" }, { name: "GVT Program", iconNum: 4, id: "d" }];
+
+  const mySetData: recommandDataType[] = [];
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <Header back run />
-        <ExploreContainer />
+        <Header title />
+        <Container bgColor>
+          <article className="homeContainer">
+            <section className="setCardBox">
+              <h1>헬뽀 추천 세트</h1>
+              <div className="cardBox">
+                {recommandData.map((obj: recommandDataType) => (
+                  <Card name={obj.name} iconNum={obj.iconNum} id={obj.id} key={obj.id} />
+                ))}
+              </div>
+            </section>
+            <section className="setCardBox">
+              <h1>내 운동 세트</h1>
+              <div className="cardBox">
+                {mySetData.map((obj: recommandDataType) => (
+                  <Card name={obj.name} iconNum={obj.iconNum} id={obj.id} key={obj.id} />
+                ))}
+                <Card name="add" iconNum={0} id=""/>
+              </div>
+            </section>
+          </article>
+        </Container>
       </IonContent>
     </IonPage>
   );
