23d58b0ce75a998428cc76593fd21dede6f0d5b7
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-06-26 04:44:33 +0900

    Card 컴포넌트 구현

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./style.scss"
+import { iconsData } from '../../static'
+import { IonIcon } from '@ionic/react'
+import { add } from 'ionicons/icons'
+import { useHistory } from 'react-router'
+
+
+interface CardProps {
+    name: string
+    iconNum: number
+    id:string
+}
+
+/**
+ * 카드
+ * @param {string} name 아이콘 이름
+ * @param {num} iconNum 아이콘 번호
+ * @returns {*} .
+ */
+
+export const Card: React.FC<CardProps> = ({ name, iconNum, id }) => {
+
+    const history = useHistory();
+
+
+    // 내 운동 추가 카드이면
+    if (name === "add") {
+        return (
+            <div className="addCard" onClick={()=>history.push("/setting/" + id)}>
+                <IonIcon icon={add} style={{ fontSize: "32px" }} />
+            </div>
+        )
+    }
+    // 아니면
+    return (
+        <div className="card" onClick={()=>history.push("/setting/" + id)}>
+            <p>{name}</p>
+            <img src={"/assets/icons/" + iconsData[iconNum - 1].name}></img>
+        </div>
+    )
+}
