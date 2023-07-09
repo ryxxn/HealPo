0d71077a378de41c0c95d47b04443485b06a00c9
박도륜 <88328436+ryxxn@users.noreply.github.com>
2023-07-09 23:43:17 +0900

    setting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'HealPo',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
